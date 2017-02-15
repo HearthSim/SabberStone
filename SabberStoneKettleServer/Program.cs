dbaa375918a059896bc9ca5546c8232c88e8ff91
darkfriend77 <darkfriend@swissonline.ch>
2017-02-15 21:34:54 +0100

    added kettle server

---
+++ SabberStoneKettleServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SabberStoneKettleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
