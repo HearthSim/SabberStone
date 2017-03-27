da93e21c8eaaac4d2491a23e9e324fd7dc7dd426
darkfriend77 <darkfriend@swissonline.ch>
2017-03-27 23:04:21 +0200

    foxed bad commit

--- SabberStoneKettleSimulator/PowerHistoryEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PowerLogInterpreter
-{
-    public abstract class PowerHistoryEntry
-    {
-        public PowerType PowerType { get; set; }
-
-        public abstract void Process(PowerGame powerGame);
-    }
-}
