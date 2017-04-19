6e1cd1cd4e6b0b9c9fbca33ad53a06a885dea3d9
darkfriend77 <darkfriend77@users.noreply.github.com>
2017-04-19 21:47:50 +0200

    Revert "IDEA: New Healtask (#21)"
    This reverts commit f45260f8831d9b25e232d8bc5466afb8a3ef7a20.

--- SabberStoneCore/Tasks/SimpleTasks/HealTask.cs
+++ SabberStoneCore/Tasks/SimpleTasks/HealTask.cs
@@ -8,76 +8,27 @@ namespace SabberStoneCore.Tasks.SimpleTasks
         {
             Amount = amount;
             Type = entityType;
-            Sort = HealType.Heal;
-        }
-
-        public HealTask(EntityType entityType)
-        {
-            Type = entityType;
-            Sort = HealType.HealNumber;
-        }
-        
-        public HealTask(int amount)
-        {
-            Amount = amount;
-            Type = EntityType.STACK;
-            Sort = HealType.HealStack;
         }
 
         public int Amount { get; set; }
 
         public EntityType Type { get; set; }
 
-        internal HealType Sort { get; set; }
-
-        internal enum HealType
-        {
-            Heal, HealNumber, HealStack 
-        }
-
         public override TaskState Process()
         {
+            if (Amount < 1)
+            {
+                return TaskState.STOP;
+            }
+
             var source = Source as IPlayable;
             var entities = IncludeTask.GetEntites(Type, Controller, Source, Target, Playables);
-            switch (Sort)
+            entities.ForEach(p =>
             {
-                case HealType.Heal:
-                    if (Amount < 1)
-                    {
-                        return TaskState.STOP;
-                    }
+                var target = p as ICharacter;
+                target?.TakeHeal(source, Amount);
+            });
 
-                    entities.ForEach(p =>
-                    {
-                        var target = p as ICharacter;
-                        target?.TakeHeal(source, Amount);
-                    });
-                    break;
-
-                case HealType.HealNumber:
-                    entities.ForEach(p =>
-                    {
-                        var target = p as ICharacter;
-                        target?.TakeHeal(source, Number);
-                    });
-                    break;
-
-                case HealType.HealStack:
-                    if ((source == null) || (Amount < 1))
-                    {
-                        return TaskState.STOP;
-                    }
-
-                    Playables.ForEach(p =>
-                    {
-                        var target = p as ICharacter;
-                        target?.TakeHeal(source, Amount);
-                    });
-                    break;
-
-                default:
-                    break;
-            }
             return TaskState.COMPLETE;
         }
 
