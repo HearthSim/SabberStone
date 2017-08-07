89f4f3febdbb0a8c4b65009e94a60a5edc96eb20
Bert Proesmans <bproesmans@hotmail.com>
2017-08-07 14:46:05 +0200

    Formatting after rebase on master

--- extensions/SabberStoneGui/Score/Score.cs
+++ extensions/SabberStoneGui/Score/Score.cs
@@ -1,8 +1,3 @@
-﻿﻿using System.Linq;
-using SabberStoneCore.Model;
-using SabberStoneCore.Model.Zones;
-using SabberStoneCore.Model.Entities.Playables;
-using SabberStoneCore.Model.Entities;
 ﻿using SabberStoneCore.Model.Entities;
 using SabberStoneCore.Model.Entities.Playables;
 using SabberStoneCore.Model.Zones;
@@ -11,55 +6,55 @@ using System.Linq;
 namespace SabberStoneCoreGui.Score
 {
 	public interface IScore
-    {
-        Controller Controller { get; set; }
-        int Rate();
-    }
+	{
+		Controller Controller { get; set; }
+		int Rate();
+	}
 
-    public class Score : IScore
-    {
-        public Controller Controller { get; set; }
+	public class Score : IScore
+	{
+		public Controller Controller { get; set; }
 
-        public int HeroHp => Controller.Hero.Health;
+		public int HeroHp => Controller.Hero.Health;
 
-        public int OpHeroHp => Controller.Opponent.Hero.Health;
+		public int OpHeroHp => Controller.Opponent.Hero.Health;
 
-        public int HeroAtk => Controller.Hero.TotalAttackDamage;
+		public int HeroAtk => Controller.Hero.TotalAttackDamage;
 
-        public int OpHeroAtk => Controller.Opponent.Hero.TotalAttackDamage;
+		public int OpHeroAtk => Controller.Opponent.Hero.TotalAttackDamage;
 
-        public Zone<IPlayable> Hand => Controller.Hand;
+		public Zone<IPlayable> Hand => Controller.Hand;
 
-        public int HandTotCost => Hand.Sum(p => p.Cost);
+		public int HandTotCost => Hand.Sum(p => p.Cost);
 
-        public int HandCnt => Controller.Hand.Count;
+		public int HandCnt => Controller.Hand.Count;
 
-        public int OpHandCnt => Controller.Opponent.Hand.Count;
+		public int OpHandCnt => Controller.Opponent.Hand.Count;
 
-        public int DeckCnt => Controller.Deck.Count;
+		public int DeckCnt => Controller.Deck.Count;
 
-        public int OpDeckCnt => Controller.Opponent.Deck.Count;
+		public int OpDeckCnt => Controller.Opponent.Deck.Count;
 
-        public Zone<Minion> Board => Controller.Board;
+		public Zone<Minion> Board => Controller.Board;
 
-        public Zone<Minion> OpBoard => Controller.Opponent.Board;
+		public Zone<Minion> OpBoard => Controller.Opponent.Board;
 
-        public int MinionTotAtk => Board.Sum(p => p.AttackDamage);
+		public int MinionTotAtk => Board.Sum(p => p.AttackDamage);
 
-        public int OpMinionTotAtk => OpBoard.Sum(p => p.AttackDamage);
+		public int OpMinionTotAtk => OpBoard.Sum(p => p.AttackDamage);
 
-        public int MinionTotHealth => Board.Sum(p => p.Health);
+		public int MinionTotHealth => Board.Sum(p => p.Health);
 
-        public int OpMinionTotHealth => OpBoard.Sum(p => p.Health);
+		public int OpMinionTotHealth => OpBoard.Sum(p => p.Health);
 
-        public int MinionTotHealthTaunt => Board.Where(p => p.HasTaunt).Sum(p => p.Health);
+		public int MinionTotHealthTaunt => Board.Where(p => p.HasTaunt).Sum(p => p.Health);
 
-        public int OpMinionTotHealthTaunt => OpBoard.Where(p => p.HasTaunt).Sum(p => p.Health);
+		public int OpMinionTotHealthTaunt => OpBoard.Where(p => p.HasTaunt).Sum(p => p.Health);
 
-        public virtual int Rate()
-        {
-            return 0;
-        }
+		public virtual int Rate()
+		{
+			return 0;
+		}
 
-    }
+	}
 }
