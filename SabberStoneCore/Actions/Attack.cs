361c99994642510cf48b2a59291229c437e84326
darkfriend77 <darkfriend@swissonline.ch>
2017-08-08 17:11:03 +0200

    some more reformatting and removed the global.json

--- SabberStoneCore/Actions/Attack.cs
+++ SabberStoneCore/Actions/Attack.cs
@@ -5,160 +5,162 @@ using SabberStoneCore.Model;
 
 namespace SabberStoneCore.Actions
 {
-    public partial class Generic
-    {
-        public static Func<Controller, ICharacter, ICharacter, bool> AttackBlock
-            => delegate(Controller c, ICharacter source, ICharacter target)
-            {
-                if (!PreAttackPhase.Invoke(c, source, target))
-                    return false;
-                if (!OnAttackTrigger.Invoke(c, source, target))
-                {
-                    // end block
-                    if (c.Game.History)
-                        c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockEnd());
-                    return false;
-                }
-                if (!AttackPhase.Invoke(c, source))
-                {
-                    // end block
-                    if (c.Game.History)
-                        c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockEnd());
-                    return false;
-                }
-                // end block
-                if (c.Game.History)
-                    c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockEnd());
-                return true;
-            };
+	public partial class Generic
+	{
+		public static Func<Controller, ICharacter, ICharacter, bool> AttackBlock
+			=> delegate (Controller c, ICharacter source, ICharacter target)
+			{
+				if (!PreAttackPhase.Invoke(c, source, target))
+					return false;
+				if (!OnAttackTrigger.Invoke(c, source, target))
+				{
+					// end block
+					if (c.Game.History)
+						c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockEnd());
+					return false;
+				}
+				if (!AttackPhase.Invoke(c, source))
+				{
+					// end block
+					if (c.Game.History)
+						c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockEnd());
+					return false;
+				}
+				// end block
+				if (c.Game.History)
+					c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockEnd());
+				return true;
+			};
 
-        private static Func<Controller, ICharacter, ICharacter, bool> PreAttackPhase
-            => delegate(Controller c, ICharacter source, ICharacter target)
-            {
-                if (c != source.Controller || c == target.Controller)
-                {
-                    c.Game.Log(LogLevel.ERROR, BlockType.ATTACK, "PreAttackPhase", "wrong controller in phase.");
-                    return false;
-                }
-                if (!source.CanAttack || !source.IsValidAttackTarget(target))
-                {
-                    c.Game.Log(LogLevel.ERROR, BlockType.ATTACK, "PreAttackPhase", "can't attack with this card.");
-                    return false;
-                }
-                var hero = source as Hero;
+		private static Func<Controller, ICharacter, ICharacter, bool> PreAttackPhase
+			=> delegate (Controller c, ICharacter source, ICharacter target)
+			{
+				if (c != source.Controller || c == target.Controller)
+				{
+					c.Game.Log(LogLevel.ERROR, BlockType.ATTACK, "PreAttackPhase", "wrong controller in phase.");
+					return false;
+				}
+				if (!source.CanAttack || !source.IsValidAttackTarget(target))
+				{
+					c.Game.Log(LogLevel.ERROR, BlockType.ATTACK, "PreAttackPhase", "can't attack with this card.");
+					return false;
+				}
+				var hero = source as Hero;
 
-                c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "PreAttackPhase", $"[PreAttackPhase]{source}[ATK:{source.AttackDamage}/HP:{source.Health}{(hero != null ? $"/ARM:{hero.Armor}" : "")}] " +
-                    $"{(hero?.Weapon != null ? $"[{hero.Weapon}[A:{hero.Weapon.AttackDamage}/D:{hero.Weapon.Durability}]] " : "")}is attacking " +
-                    $"{target}[ATK:{target.AttackDamage}/HP:{target.Health}].");
+				c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "PreAttackPhase", $"[PreAttackPhase]{source}[ATK:{source.AttackDamage}/HP:{source.Health}{(hero != null ? $"/ARM:{hero.Armor}" : "")}] " +
+					$"{(hero?.Weapon != null ? $"[{hero.Weapon}[A:{hero.Weapon.AttackDamage}/D:{hero.Weapon.Durability}]] " : "")}is attacking " +
+					$"{target}[ATK:{target.AttackDamage}/HP:{target.Health}].");
 
-                // attack block
-                if (c.Game.History)
-                    c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockStart(BlockType.ATTACK, source.Id, "", -1, target.Id));
+				// attack block
+				if (c.Game.History)
+					c.Game.PowerHistory.Add(PowerHistoryBuilder.BlockStart(BlockType.ATTACK, source.Id, "", -1, target.Id));
 
-                // TODO need to be manipulated for 50% chance to attack  someone else 
-                source.ProposedAttacker = source.Id;
-                source.ProposedDefender = target.Id;
+				// TODO need to be manipulated for 50% chance to attack  someone else 
+				source.ProposedAttacker = source.Id;
+				source.ProposedDefender = target.Id;
 
-                return true;
-            };
+				return true;
+			};
 
-        private static Func<Controller, ICharacter, ICharacter, bool> OnAttackTrigger
-            => delegate(Controller c, ICharacter source, ICharacter target)
-            {
-                source.IsAttacking = true;
-                target.IsDefending = true;
-                c.Game.DeathProcessingAndAuraUpdate();
-                if (source.IsDead || target.IsDead)
-                {
-                    c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "OnAttackTrigger", "Oh shizzle, something died to the shizzeling of triggering ...");
-                    return false;
-                }
-                return true;
-            };
+		private static Func<Controller, ICharacter, ICharacter, bool> OnAttackTrigger
+			=> delegate (Controller c, ICharacter source, ICharacter target)
+			{
+				source.IsAttacking = true;
+				target.IsDefending = true;
+				c.Game.DeathProcessingAndAuraUpdate();
+				if (source.IsDead || target.IsDead)
+				{
+					c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "OnAttackTrigger", "Oh shizzle, something died to the shizzeling of triggering ...");
+					return false;
+				}
+				return true;
+			};
 
-        private static Func<Controller, ICharacter, bool> AttackPhase
-            => delegate(Controller c, ICharacter source)
-            {
-                var hero = source as Hero;
-                var minion = source as Minion;
-                source.ProposedAttacker = source.Id;
-                var target = c.Game.IdEntityDic[source.ProposedDefender] as ICharacter;
-                if (target == null)
-                {
-                    c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "AttackPhase", "target wasn't found by proposed defender call.");
-                    return false;
-                }
-                c.Game.Step = Step.MAIN_COMBAT;
+		private static Func<Controller, ICharacter, bool> AttackPhase
+			=> delegate (Controller c, ICharacter source)
+			{
+				var hero = source as Hero;
+				var minion = source as Minion;
+				source.ProposedAttacker = source.Id;
+				var target = c.Game.IdEntityDic[source.ProposedDefender] as ICharacter;
+				if (target == null)
+				{
+					c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "AttackPhase", "target wasn't found by proposed defender call.");
+					return false;
+				}
 
-                // Save defender's attack as it might change after being damaged (e.g. enrage)
-                var targetHero = target as Hero;
-                var targetAttack = targetHero != null ? 0 : target.AttackDamage;
-                var sourceAttack = hero?.TotalAttackDamage ?? source.AttackDamage;
+				// Force the game into MAIN_COMBAT step!
+				c.Game.Step = Step.MAIN_COMBAT;
 
-                var targetDamaged = target.TakeDamage(source, sourceAttack);
+				// Save defender's attack as it might change after being damaged (e.g. enrage)
+				var targetHero = target as Hero;
+				var targetAttack = targetHero != null ? 0 : target.AttackDamage;
+				var sourceAttack = hero?.TotalAttackDamage ?? source.AttackDamage;
 
-                // freeze target if attacker is freezer
-                if (targetDamaged && minion != null && minion.Freeze)
-                {
-                    target.IsFrozen = true;
-                }
+				var targetDamaged = target.TakeDamage(source, sourceAttack);
 
-                // destroy target if attacker is poisonous
-                if (targetDamaged && targetHero == null && (minion != null && minion.Poisonous || hero?.Weapon != null && hero.Weapon.Poisonous))
-                {
-                    target.Destroy();
-                }
+				// freeze target if attacker is freezer
+				if (targetDamaged && minion != null && minion.Freeze)
+				{
+					target.IsFrozen = true;
+				}
 
-                // ignore damage from defenders with 0 attack
-                if (targetAttack > 0)
-                {
-                    var sourceDamaged = source.TakeDamage(target, targetAttack);
+				// destroy target if attacker is poisonous
+				if (targetDamaged && targetHero == null && (minion != null && minion.Poisonous || hero?.Weapon != null && hero.Weapon.Poisonous))
+				{
+					target.Destroy();
+				}
 
-                    // freeze source if defender is freezer
-                    var targetMinion = target as Minion;
-                    if (sourceDamaged && targetMinion != null && targetMinion.Freeze)
-                    {
-                        source.IsFrozen = true;
-                    }
+				// ignore damage from defenders with 0 attack
+				if (targetAttack > 0)
+				{
+					var sourceDamaged = source.TakeDamage(target, targetAttack);
 
-                    // destroy source if defender is poisonous
-                    if (sourceDamaged && targetMinion != null && targetMinion.Poisonous)
-                    {
-                        source.Destroy();
-                    }
-                }
+					// freeze source if defender is freezer
+					var targetMinion = target as Minion;
+					if (sourceDamaged && targetMinion != null && targetMinion.Freeze)
+					{
+						source.IsFrozen = true;
+					}
 
-                if (minion != null && minion.HasStealth)
-                {
-                    minion.HasStealth = false;
-                }
+					// destroy source if defender is poisonous
+					if (sourceDamaged && targetMinion != null && targetMinion.Poisonous)
+					{
+						source.Destroy();
+					}
+				}
 
-                // remove durability from weapon if hero attack
-                if (hero?.Weapon != null)
-                {
-                    hero.Weapon.Durability -= hero.Weapon.Durability > 0 ? 1 : 0;
-                }
+				if (minion != null && minion.HasStealth)
+				{
+					minion.HasStealth = false;
+				}
 
-                source.IsAttacking = false;
-                target.IsDefending = false;
+				// remove durability from weapon if hero attack
+				if (hero?.Weapon != null)
+				{
+					hero.Weapon.Durability -= hero.Weapon.Durability > 0 ? 1 : 0;
+				}
 
-                source.NumAttacksThisTurn++;
-                c.NumOptionsPlayedThisTurn++;
-                if (minion != null)
-                    c.NumFriendlyMinionsThatAttackedThisTurn++;
+				source.IsAttacking = false;
+				target.IsDefending = false;
 
-                // set exhausted ...
-                if (source.NumAttacksThisTurn > 0 && !source.HasWindfury ||
-                    source.NumAttacksThisTurn > 1 && source.HasWindfury)
-                {
-                    c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "AttackPhase", $"{source} is now exhausted.");
-                    source.IsExhausted = true;
-                }
+				source.NumAttacksThisTurn++;
+				c.NumOptionsPlayedThisTurn++;
+				if (minion != null)
+					c.NumFriendlyMinionsThatAttackedThisTurn++;
 
-                c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "AttackPhase", $"[AttackPhase]{source}[ATK:{source.AttackDamage}/HP:{source.Health}{(hero != null ? $"/ARM:{hero.Armor}" : "")}] " +
-                    $"{(hero?.Weapon != null ? $"[{hero.Weapon}[A:{hero.Weapon.AttackDamage}/D:{hero.Weapon.Durability}]] " : "")}attacked " +
-                    $"{target}[ATK:{target.AttackDamage}/HP:{target.Health}].");
-                return true;
-            };
-    }
+				// set exhausted ...
+				if (source.NumAttacksThisTurn > 0 && !source.HasWindfury ||
+					source.NumAttacksThisTurn > 1 && source.HasWindfury)
+				{
+					c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "AttackPhase", $"{source} is now exhausted.");
+					source.IsExhausted = true;
+				}
+
+				c.Game.Log(LogLevel.INFO, BlockType.ATTACK, "AttackPhase", $"[AttackPhase]{source}[ATK:{source.AttackDamage}/HP:{source.Health}{(hero != null ? $"/ARM:{hero.Armor}" : "")}] " +
+					$"{(hero?.Weapon != null ? $"[{hero.Weapon}[A:{hero.Weapon.AttackDamage}/D:{hero.Weapon.Durability}]] " : "")}attacked " +
+					$"{target}[ATK:{target.AttackDamage}/HP:{target.Health}].");
+				return true;
+			};
+	}
 }
--- SabberStoneCore/Config/GameConfig.cs
+++ SabberStoneCore/Config/GameConfig.cs
@@ -4,41 +4,41 @@ using SabberStoneCore.Model;
 
 namespace SabberStoneCore.Config
 {
-    public class GameConfig
-    {
-        public int StartPlayer { get; set; } = -1;
+	public class GameConfig
+	{
+		public int StartPlayer { get; set; } = -1;
 
-        public string Player1Name { get; set; } = "Player1";
+		public string Player1Name { get; set; } = "Player1";
 
-        public CardClass Player1HeroClass { get; set; } = CardClass.HUNTER;
+		public CardClass Player1HeroClass { get; set; } = CardClass.HUNTER;
 
-        public string Player2Name { get; set; } = "Player2";
+		public string Player2Name { get; set; } = "Player2";
 
-        public CardClass Player2HeroClass { get; set; } = CardClass.MAGE;
+		public CardClass Player2HeroClass { get; set; } = CardClass.MAGE;
 
-        public FormatType GameRule { get; set; } = FormatType.FT_STANDARD;
+		public FormatType GameRule { get; set; } = FormatType.FT_STANDARD;
 
-        public List<Card> DeckPlayer1 { get; set; } = null;
+		public List<Card> DeckPlayer1 { get; set; } = null;
 
-        public List<Card> DeckPlayer2 { get; set; } = null;
+		public List<Card> DeckPlayer2 { get; set; } = null;
 
-        public bool FillDecks { get; set; } = false;
+		public bool FillDecks { get; set; } = false;
 
-        public bool DrawPool { get; set; } = false;
+		public bool DrawPool { get; set; } = false;
 
-        public bool Shuffle { get; set; } = true;
+		public bool Shuffle { get; set; } = true;
 
-        public bool Splitting { get; set; } = false;
+		public bool Splitting { get; set; } = false;
 
-        public bool Logging { get; set; } = true;
+		public bool Logging { get; set; } = true;
 
-        public bool History { get; set; } = true;
+		public bool History { get; set; } = true;
 
-        public bool SkipMulligan { get; set; } = true;
+		public bool SkipMulligan { get; set; } = true;
 
-        public GameConfig Clone()
-        {
-            return (GameConfig) this.MemberwiseClone();
-        }
-    }
+		public GameConfig Clone()
+		{
+			return (GameConfig)this.MemberwiseClone();
+		}
+	}
 }
