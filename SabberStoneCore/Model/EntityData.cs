42d8c794fed81c17744c5fd0253cdc00b5f001cf
darkfriend77 <darkfriend@swissonline.ch>
2017-08-08 16:00:49 +0200

    refactored controller, entity & entitydata

--- SabberStoneCore/Model/EntityData.cs
+++ SabberStoneCore/Model/EntityData.cs
@@ -7,94 +7,123 @@ using SabberStoneCore.Enums;
 
 namespace SabberStoneCore.Model
 {
-    internal class EntityData : IEnumerable<KeyValuePair<GameTag, int>>
-    {
-        internal EntityData(Card card, Dictionary<GameTag, int> tags)
-        {
-            Card = card;
-            Tags = tags ?? new Dictionary<GameTag, int>(Enum.GetNames(typeof(GameTag)).Length);
-        }
+	/// <summary>
+	/// Holds original entity data for a specific card.
+	/// </summary>
+	internal class EntityData : IEnumerable<KeyValuePair<GameTag, int>>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="EntityData"/> class.
+		/// </summary>
+		/// <param name="card"></param>
+		/// <param name="tags"></param>
+		internal EntityData(Card card, Dictionary<GameTag, int> tags)
+		{
+			Card = card;
+			Tags = tags ?? new Dictionary<GameTag, int>(Enum.GetNames(typeof(GameTag)).Length);
+		}
 
-        public void Stamp(EntityData data)
-        {
-            Tags = new Dictionary<GameTag, int>(data.Tags);
-        }
+		/// <summary>
+		/// Copies data from the other object into this one.
+		/// </summary>
+		/// <param name="data">The data.</param>
+		public void Stamp(EntityData data)
+		{
+			Tags = new Dictionary<GameTag, int>(data.Tags);
+		}
 
-        public StringBuilder Hash(params GameTag[] ignore)
-        {
-            var hash = new StringBuilder();
-            hash.Append("[");
-            hash.Append(Card.Id);
-            hash.Append("][GT:");
-            var keys = Tags.Keys.ToList();
-            keys.Sort();
-            keys.ForEach(p =>
-            {
-                if (!ignore.Contains(p))
-                {
-                    hash.Append("{");
-                    hash.Append(p.ToString());
-                    hash.Append(",");
-                    hash.Append(Tags[p].ToString());
-                    hash.Append("}");
-                }
-            });
-            hash.Append("]");
-            return hash;
-        }
+		/// <summary>
+		/// Returns a string uniquely identifying this object.
+		/// </summary>
+		/// <param name="ignore">The tags to ignore during hashing.</param>
+		/// <returns>The hash string.</returns>
+		public string Hash(params GameTag[] ignore)
+		{
+			var hash = new StringBuilder();
+			hash.Append("[");
+			hash.Append(Card.Id);
+			hash.Append("][GT:");
+			var keys = Tags.Keys.ToList();
+			keys.Sort();
+			keys.ForEach(p =>
+			{
+				if (!ignore.Contains(p))
+				{
+					hash.Append("{");
+					hash.Append(p.ToString());
+					hash.Append(",");
+					hash.Append(Tags[p].ToString());
+					hash.Append("}");
+				}
+			});
+			hash.Append("]");
+			return hash.ToString();
+		}
 
-        public Card Card { get; }
+		/// <summary>
+		/// The card which was used to create this instance.
+		/// </summary>
+		public Card Card { get; }
 
-        public Dictionary<GameTag, int> Tags { get; private set; }
+		/// <summary>
+		/// The container with tags used to create this instance.
+		/// </summary>
+		/// TODO; This might be made private.
+		public Dictionary<GameTag, int> Tags { get; private set; }
 
-        public int this[GameTag t]
-        {
-            get { return Tags.ContainsKey(t) ? Tags[t] : (Card.Tags.ContainsKey(t) ? Card[t] : 0); }
-            set { Tags[t] = value; }
-        }
+		/// <summary>Gets or sets the value of a specific GameTag.</summary>
+		/// <value></value>
+		/// <param name="t">The GameTag.</param>
+		/// <returns></returns>
+		public int this[GameTag t]
+		{
+			get { return Tags.ContainsKey(t) ? Tags[t] : (Card.Tags.ContainsKey(t) ? Card[t] : 0); }
+			set { Tags[t] = value; }
+		}
 
-        public IEnumerator<KeyValuePair<GameTag, int>> GetEnumerator()
-        {
-            // Entity ID
-            var allTags = new Dictionary<GameTag, int>(Card.Tags);
+		public IEnumerator<KeyValuePair<GameTag, int>> GetEnumerator()
+		{
+			// Entity ID
+			var allTags = new Dictionary<GameTag, int>(Card.Tags);
 
-            // Entity tags override card tags
-            foreach (var tag in Tags)
-            {
-                allTags[tag.Key] = tag.Value;
-            }
+			// Entity tags override card tags
+			foreach (KeyValuePair<GameTag, int> tag in Tags)
+			{
+				allTags[tag.Key] = tag.Value;
+			}
 
-            return allTags.GetEnumerator();
-        }
+			return allTags.GetEnumerator();
+		}
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
 
-        public override string ToString()
-        {
-            var s = Tags.Aggregate(Card.Name + " - ", (current, tag) => current + new Tag(tag.Key, tag.Value) + ", ");
-            return s.Substring(0, s.Length - 2);
-        }
+		public override string ToString()
+		{
+			string s = Tags.Aggregate(Card.Name + " - ", (current, tag) => current + new Tag(tag.Key, tag.Value) + ", ");
+			return s.Substring(0, s.Length - 2);
+		}
 
-        public void Reset(Dictionary<GameTag, int> tags = null)
-        {
-            //Tags = tags ?? new Dictionary<GameTag, int>(Enum.GetNames(typeof(GameTag)).Length);
-            Tags.Remove(GameTag.ATK);
-            Tags.Remove(GameTag.HEALTH);
-            Tags.Remove(GameTag.COST);
-            Tags.Remove(GameTag.DAMAGE);
-            Tags.Remove(GameTag.TAUNT);
-            Tags.Remove(GameTag.FROZEN);
-            Tags.Remove(GameTag.ENRAGED);
-            Tags.Remove(GameTag.CHARGE);
-            Tags.Remove(GameTag.WINDFURY);
-            Tags.Remove(GameTag.DIVINE_SHIELD);
-            Tags.Remove(GameTag.STEALTH);
-            Tags.Remove(GameTag.DEATHRATTLE);
-            Tags.Remove(GameTag.BATTLECRY);
-            Tags.Remove(GameTag.SILENCED);
-        }
-    }
+		/// <summary>Resets all tags from the container.</summary>
+		public void Reset(Dictionary<GameTag, int> tags = null)
+		{
+			//Tags = tags ?? new Dictionary<GameTag, int>(Enum.GetNames(typeof(GameTag)).Length);
+			Tags.Remove(GameTag.ATK);
+			Tags.Remove(GameTag.HEALTH);
+			Tags.Remove(GameTag.COST);
+			Tags.Remove(GameTag.DAMAGE);
+			Tags.Remove(GameTag.TAUNT);
+			Tags.Remove(GameTag.FROZEN);
+			Tags.Remove(GameTag.ENRAGED);
+			Tags.Remove(GameTag.CHARGE);
+			Tags.Remove(GameTag.WINDFURY);
+			Tags.Remove(GameTag.DIVINE_SHIELD);
+			Tags.Remove(GameTag.STEALTH);
+			Tags.Remove(GameTag.DEATHRATTLE);
+			Tags.Remove(GameTag.BATTLECRY);
+			Tags.Remove(GameTag.SILENCED);
+		}
+	}
 }
