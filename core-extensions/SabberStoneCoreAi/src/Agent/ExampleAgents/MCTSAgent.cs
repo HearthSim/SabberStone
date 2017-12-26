419ddf830e97758af4962ac8514c6d6da189f708
ADockhorn <alexander.dockhorn@ovgu.de>
2017-12-26 16:48:42 +0100

    Added AbstractAgent and RandomAgent MCTSAgentStub

---
+++ core-extensions/SabberStoneCoreAi/src/Agent/ExampleAgents/MCTSAgent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SabberStoneCore.Tasks;
+using SabberStoneCoreAi.src.POGame;
+
+namespace SabberStoneCoreAi.src.Agent.ExampleAgents
+{
+	class MCTSAgent : AbstractAgent
+	{
+		public override void InitializeAgent()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void InitializeGame()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override List<PlayerTask> GetMove(PartialObservationGame poGame)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void FinalizeGame()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void FinalizeAgent()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
