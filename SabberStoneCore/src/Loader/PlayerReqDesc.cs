a380fde309127c6984394eedd19ebc44365928f2
darkfriend77 <darkfriend@swissonline.ch>
2018-12-15 17:41:20 +0100

    fix header files

--- SabberStoneCore/src/Loader/PlayerReqDesc.cs
+++ SabberStoneCore/src/Loader/PlayerReqDesc.cs
@@ -1,14 +1,15 @@
 ﻿#region copyright
+// SabberStone, Hearthstone Simulator in C# .NET Core
 // Copyright (C) 2017-2019 SabberStone Team, darkfriend77 & rnilva
 //
 // SabberStone is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License,
-// or any later version.
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License.
 // SabberStone is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
 #endregion
 namespace SabberStoneCore.Loader
 {
