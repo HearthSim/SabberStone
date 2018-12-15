fa55e3f3bf78e61386a482b688a39dc25066d2d3
darkfriend77 <darkfriend@swissonline.ch>
2018-12-15 15:18:58 +0100

    added license header

--- SabberStoneCore/src/Tasks/SimpleTasks/GetGameTagTask.cs
+++ SabberStoneCore/src/Tasks/SimpleTasks/GetGameTagTask.cs
@@ -1,4 +1,16 @@
-﻿using SabberStoneCore.Enums;
+﻿#region copyright
+// Copyright (C) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+//
+// SabberStone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License,
+// or any later version.
+// SabberStone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#endregion
+using SabberStoneCore.Enums;
 using System.Collections.Generic;
 using SabberStoneCore.Model.Entities;
 
--- core-extensions/SabberStonePowerLog/src/Model/PowerFullEntity.cs
+++ core-extensions/SabberStonePowerLog/src/Model/PowerFullEntity.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿#region copyright
+// Copyright (C) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+//
+// SabberStone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License,
+// or any later version.
+// SabberStone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#endregion
+using System;
 using System.Text;
 
 namespace SabberStonePowerLog.Model
